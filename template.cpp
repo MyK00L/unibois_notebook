6916e60bbed8429cdd46539228fa66b494d8440c
Michael Chelli <michaelchelli00@gmail.com>
2020-10-29 14:09:10 +0100

    added treaps

---
+++ template.cpp
@@ -0,0 +1,34 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("inline")
+#pragma GCC optimize("inline-small-functions")
+#pragma GCC optimize("-finline-small-functions")
+#pragma GCC optimize("inline-functions-called-once")
+//#pragma GCC optimize("-ffast-math")
+//#pragma GCC optimize("unroll-loops")
+//#pragma GCC optimize("-fexpensive-optimizations")
+//#pragma GCC optimize("-funsafe-loop-optimizations")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using u64 = unsigned long long;
+using i64 = long long;
+using u32 = unsigned int;
+using i32 = int;
+using u16 = unsigned short;
+using i16 = short;
+using u8 = unsigned char;
+using i8 = char;
+using usize = size_t;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main(){
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+
+	return 0;
+}
+
